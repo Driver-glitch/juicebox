1ffd0e39bb872a3b87602a18613d1b7e22bac6fc
Daniel Rivera <85755386+Driver-glitch@users.noreply.github.com>
2021-10-13 17:24:37 -0400

    seed.js finished

--- db/index.js
+++ db/index.js
@@ -2,9 +2,20 @@
 const { Client } = require('pg'); // imports the pg module
 const password = 'dan'
 // supply the db name and location of the database
-const client = new Client(`postgres://postgres:${password}@localhost:5432/juicebox-dev`);
+const client = new Client(`postgres://postgres:${password}@localhost:5433/juicebox-dev`);
 // 'postgres://username:password@localhost/database_name`
+
+async function getAllUsers() {
+  const { rows } = await client.query(
+    `SELECT id, username 
+    FROM users;
+  `);
+
+  return rows;
+}
+
 
 module.exports = {
   client,
+  getAllUsers,
 }
